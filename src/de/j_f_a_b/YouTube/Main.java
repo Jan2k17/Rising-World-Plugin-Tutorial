168cfdebeaf453213b905f2918bc716aef132961
Jan2k17 <56897434+Jan2k17@users.noreply.github.com>
2022-04-05 15:56:13 +0200

    Part2

--- src/de/j_f_a_b/YouTube/Main.java
+++ src/de/j_f_a_b/YouTube/Main.java
@@ -4,6 +4,7 @@ import net.risingworld.api.Plugin;
 import net.risingworld.api.events.EventMethod;
 import net.risingworld.api.events.Listener;
 import net.risingworld.api.events.player.PlayerCommandEvent;
+import net.risingworld.api.objects.Item;
 import net.risingworld.api.objects.Player;
 
 public class Main extends Plugin implements Listener{
@@ -21,8 +22,9 @@ public class Main extends Plugin implements Listener{
 	}
 	
 	@EventMethod
-	public void onCommand(PlayerCommandEvent e){
+	public void onCommand(PlayerCommandEvent e) {
 		Player p = e.getPlayer();
+		
 		String command = e.getCommand();
 		String[] cmd = command.split(" ");
 		
@@ -30,11 +32,29 @@ public class Main extends Plugin implements Listener{
 			if(p.isAdmin()) {
 				if(p.isFlying()) {
 					p.setFlying(false);
-					p.sendTextMessage("Du kannst nun nicht mehr fliegen!");
+					
+					p.sendTextMessage("[YouTube] Du kannst nun nicht mehr fliegen!");
 				} else {
 					p.setFlying(true);
-					p.sendTextMessage("Du kannst nun fliegen!");
+					
+					p.sendTextMessage("[YouTube] Du kannst nun fliegen!");
 				}
+			} else {
+				p.sendTextMessage("Du darfst das nicht!");
+			}
+		} else if(cmd[0].equalsIgnoreCase("/more")) {
+			if(p.isAdmin()) {
+				if(cmd.length == 2) {
+					Item item = p.getEquippedItem();
+					int amount = Integer.parseInt(cmd[1]);
+					
+					p.getInventory().insertNewItem(item.getTypeID(), item.getVariation(), amount);
+					p.sendTextMessage("[YouTube] Du hast " + amount + " von deinem ausgewähltem Item erhalten!");
+				} else {
+					p.sendTextMessage("[YouTube] Usage: /more <amount>");
+				}
+			} else {
+				p.sendTextMessage("Du darfst das nicht!");
 			}
 		}
 	}
